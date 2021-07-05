999ed18fde728768ac441658444f175fe8506cd4
deep1man3 <pasha26k@ya.ru>
2021-07-05 14:24:05 +0300

    Перемещает компонент CssBaseline из Router.tsx в App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { CssBaseline } from '@material-ui/core';
 import React from 'react';
 import Router from './components/Router';
 
-const App = () => <Router />;
+const App = () => (
+  <>
+    <CssBaseline />
+    <Router />
+  </>
+);
 
 export default App;
--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,19 +1,15 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { CssBaseline } from '@material-ui/core';
 import Home from '../../pages/Home';
 
 const Router: FC = () => (
-  <>
-    <CssBaseline />
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  </>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Router;
