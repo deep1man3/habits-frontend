41aba77e7dfab2e787835283d662857710218742
deep1man3 <pasha26k@ya.ru>
2021-07-05 11:15:50 +0300

    Изменяет содержимое и стили компонента AppBar в соответствии  с дизайном

---
+++ src/components/AppBar/AppBar.styles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles(() => ({
+  root: {
+    boxShadow: 'none',
+  },
+  toolbar: {
+    minHeight: '56px',
+  },
+  logo: {
+    textTransform: 'uppercase',
+  },
+}));
--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,12 +1,37 @@
 import React, { FC } from 'react';
-import { Toolbar, Typography, AppBar as MuiAppBar } from '@material-ui/core';
+import {
+  Toolbar,
+  Typography,
+  AppBar as MuiAppBar,
+  Grid,
+  Box,
+} from '@material-ui/core';
+import { useStyles } from './AppBar.styles';
+import Link from '../Link';
 
-const AppBar: FC = () => (
-  <MuiAppBar position="static">
-    <Toolbar>
-      <Typography variant="h6">Habits</Typography>
-    </Toolbar>
-  </MuiAppBar>
-);
+const AppBar: FC = () => {
+  const classes = useStyles();
 
+  return (
+    <MuiAppBar position="static" className={classes.root}>
+      <Toolbar className={classes.toolbar}>
+        <Grid container justify="space-between" alignItems="center">
+          <Grid item>
+            <Typography variant="h6" className={classes.logo}>
+              Habits
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Box component="span" mr={2}>
+              <Link to="/sign-in" label="Войти" />
+            </Box>
+            <Box component="span">
+              <Link to="/sign-up" label="Зарегистрироваться" />
+            </Box>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </MuiAppBar>
+  );
+};
 export default AppBar;
