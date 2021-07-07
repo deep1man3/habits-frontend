59db056faa2f12bf41aafd63796dfe0e32ca59ba
deep1man3 <pasha26k@ya.ru>
2021-07-07 11:14:52 +0300

    Убирает кнопку с формы и пробрасывает название кнопки в FormWrapper

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import {
-  Button,
   Grid,
   IconButton,
   InputAdornment,
@@ -15,56 +14,44 @@ const SignIn = () => {
 
   return (
     <>
-      <FormWrapper>
-        <Grid item container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h4" align="center" color="primary">
-              Авторизация
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="email"
-              label="Ваш email"
-              autoComplete="email"
-              variant="outlined"
-              required
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="password"
-              label="Ваш пароль"
-              autoComplete="password"
-              variant="outlined"
-              type={showPassword ? 'text' : 'password'}
-              required
-              fullWidth
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() => setShowPassword((prev) => !prev)}
-                      edge="end"
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              variant="outlined"
-              color="primary"
-              disableElevation
-              fullWidth
-            >
-              Войти
-            </Button>
-          </Grid>
+      <FormWrapper buttonLabel="Войти">
+        <Grid item xs={12}>
+          <Typography variant="h4" align="center" color="primary">
+            Авторизация
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="email"
+            label="Ваш email"
+            autoComplete="email"
+            variant="outlined"
+            required
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="password"
+            label="Ваш пароль"
+            autoComplete="password"
+            variant="outlined"
+            type={showPassword ? 'text' : 'password'}
+            required
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => setShowPassword((prev) => !prev)}
+                    edge="end"
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
         </Grid>
       </FormWrapper>
     </>
