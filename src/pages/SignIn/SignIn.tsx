0e49b8ca315352789d871efc365f3c553a7863c0
deep1man3 <pasha26k@ya.ru>
2021-07-07 12:10:05 +0300

    Добавляет форму авторизации с контролем от react-hook-form

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -7,52 +7,78 @@ import {
   Typography,
 } from '@material-ui/core';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
+import { useForm, Controller } from 'react-hook-form';
 import FormWrapper from '../../components/lowLevel/FormWrapper';
+import Button from '../../components/lowLevel/Button';
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const { control, handleSubmit } = useForm();
 
   return (
     <>
-      <FormWrapper buttonLabel="Войти">
-        <Grid item xs={12}>
-          <Typography variant="h4" align="center" color="primary">
-            Авторизация
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="email"
-            label="Ваш email"
-            autoComplete="email"
-            variant="outlined"
-            required
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="password"
-            label="Ваш пароль"
-            autoComplete="password"
-            variant="outlined"
-            type={showPassword ? 'text' : 'password'}
-            required
-            fullWidth
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={() => setShowPassword((prev) => !prev)}
-                    edge="end"
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
+      <FormWrapper>
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h4" align="center" color="primary">
+                Авторизация
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                control={control}
+                defaultValue=""
+                name="email"
+                render={({ field }) => (
+                  <TextField
+                    // eslint-disable-next-line
+                    {...field}
+                    id="email"
+                    label="Ваш email"
+                    autoComplete="email"
+                    variant="outlined"
+                    required
+                    fullWidth
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                control={control}
+                defaultValue=""
+                name="password"
+                render={({ field }) => (
+                  <TextField
+                    // eslint-disable-next-line
+                    {...field}
+                    id="password"
+                    label="Ваш пароль"
+                    autoComplete="password"
+                    variant="outlined"
+                    type={showPassword ? 'text' : 'password'}
+                    required
+                    fullWidth
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={() => setShowPassword((prev) => !prev)}
+                            edge="end"
+                          >
+                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                )}
+              />
+            </Grid>
+            <Button>Войти</Button>
+          </Grid>
+        </form>
       </FormWrapper>
     </>
   );
