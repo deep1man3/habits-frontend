e1f01b8676983b9d508c55d474264c007f3ff46b
deep1man3 <pasha26k@ya.ru>
2021-07-07 10:59:59 +0300

    Добавляет параметры maxWidth,boxShadow для кастомизации компонента FormWrapper

--- src/components/lowLevel/FormWrapper/FormWrapper.styles.ts
+++ src/components/lowLevel/FormWrapper/FormWrapper.styles.ts
@@ -1,9 +1,14 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useStyles = (maxWidth: number) =>
+export const useStyles = (maxWidth: number, boxShadow: string) =>
   makeStyles((theme) => ({
+    root: {
+      height: '100%',
+    },
     form: {
       padding: theme.spacing(2),
+      border: '1px solid #E0E0E0',
       maxWidth,
+      boxShadow,
     },
   }));
--- src/components/lowLevel/FormWrapper/FormWrapper.tsx
+++ src/components/lowLevel/FormWrapper/FormWrapper.tsx
@@ -1,13 +1,30 @@
-import { Paper } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 import React, { PropsWithChildren } from 'react';
 import { useStyles } from './FormWrapper.styles';
 
-interface FormWrapperProps {}
+interface FormWrapperProps {
+  maxWidth?: number;
+  boxShadow?: string;
+}
 
-const FormWrapper = ({ children }: PropsWithChildren<FormWrapperProps>) => {
-  const classes = useStyles(480)();
+const FormWrapper = ({
+  maxWidth = 480,
+  boxShadow = 'none',
+  children,
+}: PropsWithChildren<FormWrapperProps>) => {
+  const classes = useStyles(maxWidth, boxShadow)();
 
-  return <Paper className={classes.form}>{children}</Paper>;
+  return (
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      alignContent="center"
+      className={classes.root}
+    >
+      <Paper className={classes.form}>{children}</Paper>
+    </Grid>
+  );
 };
 
 export default FormWrapper;
