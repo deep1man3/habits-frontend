caca865e043ac7fbdffb73bfcd262dcf4379d373
deep1man3 <pasha26k@ya.ru>
2021-07-07 10:42:13 +0300

    Добавляет в базовый шаблон стили для main части

--- src/components/templates/Base/Base.template.tsx
+++ src/components/templates/Base/Base.template.tsx
@@ -1,13 +1,19 @@
 import React, { PropsWithChildren } from 'react';
+import { Box } from '@material-ui/core';
 import AppBar from './AppBar';
+import { useStyles } from './Base.styles';
 
 interface BaseTemplateProps {}
 
-const BaseTemplate = ({ children }: PropsWithChildren<BaseTemplateProps>) => (
-  <>
-    <AppBar />
-    {children}
-  </>
-);
+const BaseTemplate = ({ children }: PropsWithChildren<BaseTemplateProps>) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar />
+      <Box className={classes.main}>{children}</Box>
+    </>
+  );
+};
 
 export default BaseTemplate;
