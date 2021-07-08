2b31aa3cf47747e10146aff5ef7ac5a522e24f38
deep1man3 <pasha26k@ya.ru>
2021-07-08 09:35:08 +0300

    Добавляет типизацию для `useSelector`

---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import {
+  useSelector as useReduxSelector,
+  TypedUseSelectorHook,
+} from 'react-redux';
+
+import { RootState } from './configureStore';
+
+export { default } from './configureStore';
+
+export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
