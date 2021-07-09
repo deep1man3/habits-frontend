558211e7427fd42c63ec65edb5e8348430de41bb
Pavel Lozhkin <pasha26k@ya.ru>
2021-07-09 23:32:32 +0300

    Добавляет роуты с шаблонами

--- src/routing/Router/Router.tsx
+++ src/routing/Router/Router.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Template from '../../components/templates';
+import BaseTemplate from '../../components/templates/Base';
 import Home from '../../pages/Home';
 import SignIn from '../../pages/SignIn';
 import SignUp from '../../pages/SignUp';
+import DashboardTemplate from '../../components/templates/Dashboard';
+import Dashboard from '../../pages/Dashboard';
 
 const Router = () => (
   <BrowserRouter>
-    <Template>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/sign-in">
-          <SignIn />
-        </Route>
-        <Route exact path="/sign-up">
-          <SignUp />
-        </Route>
-
-        <Route>
-          <h1>NO MATCH</h1>
-        </Route>
-      </Switch>
-    </Template>
+    <Switch>
+      <Route path="/dashboard/:path?" exact>
+        <DashboardTemplate>
+          <Switch>
+            <Route path="/dashboard" exact render={() => <Dashboard />} />
+            <Route path="/*" exact render={() => <div>404</div>} />
+          </Switch>
+        </DashboardTemplate>
+      </Route>
+      <Route>
+        <BaseTemplate>
+          <Switch>
+            <Route path="/" exact render={() => <Home />} />
+            <Route path="/sign-in" exact render={() => <SignIn />} />
+            <Route path="/sign-up" exact render={() => <SignUp />} />
+            <Route path="/*" exact render={() => <div>404</div>} />
+          </Switch>
+        </BaseTemplate>
+      </Route>
+    </Switch>
   </BrowserRouter>
 );
 
