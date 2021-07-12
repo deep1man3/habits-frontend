562069d394f03e6a5e7af980ec8c87186f62d3e3
deep1man3 <pasha26k@ya.ru>
2021-07-12 10:59:44 +0300

    Добавляет компонент Drawer

---
+++ src/components/lowLevel/Drawer/Drawer.styles.ts
@@ -0,0 +1,7 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyle = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(2),
+  },
+}));
---
+++ src/components/lowLevel/Drawer/Drawer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Drawer as MuiDrawer } from '@material-ui/core';
+import { useStyle } from './Drawer.styles';
+
+const Drawer = () => {
+  const classes = useStyle();
+
+  return <MuiDrawer>1</MuiDrawer>;
+};
+
+export default Drawer;
