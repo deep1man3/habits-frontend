a5c7855663bd2b2d59da157a6be15e1f1f799ac4
deep1man3 <pasha26k@ya.ru>
2021-07-12 13:34:46 +0300

    Добавляет возможность кастомизировать AppBar

--- src/components/lowLevel/AppBar/AppBar.tsx
+++ src/components/lowLevel/AppBar/AppBar.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import { Toolbar, AppBar as MuiAppBar, Grid, Box } from '@material-ui/core';
+import React, { PropsWithChildren } from 'react';
+import {
+  Toolbar,
+  AppBar as MuiAppBar,
+  Grid,
+  Box,
+  AppBarProps,
+} from '@material-ui/core';
+import clsx from 'clsx';
 import { useStyles } from './AppBar.styles';
-import { Link } from '../../../../routing';
-import Logo from '../../../lowLevel/Logo';
+import { Link } from '../../../routing';
+import Logo from '../Logo';
 
-const AppBar = () => {
+const AppBar = ({
+  className,
+  children,
+  ...rest
+}: PropsWithChildren<AppBarProps>) => {
   const classes = useStyles();
 
   return (
-    <MuiAppBar position="static" className={classes.root}>
+    <MuiAppBar
+      className={clsx(classes.root, className)}
+      position="static"
+      {...rest}
+    >
       <Toolbar className={classes.toolbar}>
         <Grid container justify="space-between" alignItems="center">
           <Grid item>
             <Logo />
           </Grid>
           <Grid item>
+            {children}
             <Box component="span" mr={2}>
               <Link to="/sign-in" label="Войти" />
             </Box>
