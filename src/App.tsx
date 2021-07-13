dd432c6169cdd1bc78001aae18bc4fb09672f23f
deep1man3 <pasha26k@ya.ru>
2021-07-13 17:05:35 +0300

    Добавляет useEffect при mounting приложения, который удаляет все с localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { Provider } from 'react-redux';
 import store from './store';
 import Router from './routing';
 
-const App = () => (
-  <Provider store={store}>
-    <CssBaseline />
-    <Router />
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <CssBaseline />
+      <Router />
+    </Provider>
+  );
+};
 
 export default App;
