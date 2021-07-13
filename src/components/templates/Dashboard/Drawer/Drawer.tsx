346c4eb3968fcbfa6d512f3576ccaa9d77f32617
deep1man3 <pasha26k@ya.ru>
2021-07-13 11:21:55 +0300

    Добавляет мобильную адаптивность для Drawer

--- src/components/templates/Dashboard/Drawer/Drawer.tsx
+++ src/components/templates/Dashboard/Drawer/Drawer.tsx
@@ -1,17 +1,32 @@
 import React, { PropsWithChildren } from 'react';
-import { Drawer as MuiDrawer, DrawerProps } from '@material-ui/core';
+import {
+  Drawer as MuiDrawer,
+  DrawerProps,
+  useMediaQuery,
+  useTheme,
+} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 import { useStyles } from './Drawer.styles';
+import { useSelector } from '../../../../store';
+import { metaActions } from '../../../../store/meta/slice';
 
 const Drawer = ({ children, ...rest }: PropsWithChildren<DrawerProps>) => {
   const classes = useStyles();
+  const theme = useTheme();
+  const isNotMobile = useMediaQuery(theme.breakpoints.up('md'));
+  const isLarge = useMediaQuery(theme.breakpoints.up('xl'));
+
+  const dispatch = useDispatch();
+  const isDrawerOpen = useSelector((state) => state.meta.isDrawerOpen);
 
   return (
     <MuiDrawer
       classes={{
         paper: classes.drawerContent,
       }}
-      variant="permanent"
-      open
+      variant={isNotMobile ? 'persistent' : 'temporary'}
+      open={isLarge ? true : isDrawerOpen}
+      onClose={() => dispatch(metaActions.toggleDrawerOpen())}
       {...rest}
     >
       {children}
