1c17ea32a38e1fa25e5f93d80f8984cc44909ac5
deep1man3 <pasha26k@ya.ru>
2021-07-13 17:23:35 +0300

    Добавляет отрисовку `ListItem` по полученным Tasks

--- src/components/lowLevel/TasksList/TasksList.tsx
+++ src/components/lowLevel/TasksList/TasksList.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react';
 import { Grid } from '@material-ui/core';
+import TaskItem from '../TaskItem';
 import TaskService from '../../../utils/services/taskService';
+import { useSelector } from '../../../store';
 
 const TasksList = () => {
+  const { tasks } = useSelector((state) => state.tasks);
+
   useEffect(() => {
     TaskService.getTasks(() => console.log('Задачи загрузились'));
-  });
+  }, []);
 
   return (
     <Grid container justify="center">
-      {/* {tasks && tasks.length > 0 && tasks.map((task) => <TaskItem />)} */}
+      {tasks &&
+        tasks.length > 0 &&
+        tasks.map((task) => <TaskItem key={task.id} />)}
     </Grid>
   );
 };
