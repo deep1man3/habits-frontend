2b102465a74eb3eea3dadb1137ff52cf12944a5d
deep1man3 <pasha26k@ya.ru>
2021-07-13 14:21:07 +0300

    Добавляет компоненты-иконки `CommonTaskIcon` и `WithProgressionTaskIcon`

---
+++ src/components/assets/CommonTaskIcon/CommonTaskIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CommonTaskIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" height="35" width="82">
+    <path
+      fill="#323232"
+      d="M82.333 17.5L65 0v13.125H0v8.75h65V35l17.333-17.5z"
+    />
+  </svg>
+);
+
+export default CommonTaskIcon;
---
+++ src/components/assets/WithProgressionTaskIcon/WithProgressionTaskIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const withProgressionTaskIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 95 63"
+    height="94"
+    width="61"
+  >
+    <path
+      fill="#323232"
+      d="M7.48 62.057L35.73 33.76l18.833 18.833 40.02-45.011L87.945.943 54.563 38.468 35.728 19.635.417 54.995l7.062 7.062z"
+    />
+  </svg>
+);
+
+export default withProgressionTaskIcon;
