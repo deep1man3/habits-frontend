bb78938b0a1a91c1c14acaac3429766c27e15b41
deep1man3 <pasha26k@ya.ru>
2021-07-13 17:22:10 +0300

    Изменяет initialState

--- src/store/tasks/slice.ts
+++ src/store/tasks/slice.ts
@@ -1,14 +1,16 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Task, TasksState } from '../../types/tasks.types';
 
-const initialState: TasksState = [];
+const initialState: TasksState = {
+  tasks: [],
+};
 
 const slice = createSlice({
   name: 'tasks',
   initialState,
   reducers: {
     setTasks(state, { payload }: PayloadAction<Task[]>) {
-      state = payload;
+      state.tasks = payload;
     },
   },
 });
