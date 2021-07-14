50aa526f8a02c505cda3046c9b2baff87acbcf41
deep1man3 <pasha26k@ya.ru>
2021-07-14 11:37:55 +0300

    Добавляет метод `updateTask` для TaskService

--- src/utils/services/taskService/TaskService.ts
+++ src/utils/services/taskService/TaskService.ts
@@ -2,6 +2,7 @@ import $api from '../../api';
 import { TaskResponseDTO } from '../../../types/DTO/TaskResponseDTO';
 import store from '../../../store';
 import { tasksActions } from '../../../store/tasks/slice';
+import { Task } from '../../../types/tasks.types';
 
 export default class TaskService {
   static async getTasks(
@@ -14,5 +15,15 @@ export default class TaskService {
 
     store.dispatch(tasksActions.setTasks(response.data));
     onSuccess && onSuccess();
+  }
+
+  static async updateTask(data: Task, onSuccess?: () => void): Promise<void> {
+    await $api
+      .put(`/tasks/${data.id}/update`, data)
+      .then(() => {
+        this.getTasks();
+        onSuccess && onSuccess();
+      })
+      .catch((e) => console.error('TaskService.updateTask:', e));
   }
 }
