9fa521d92262320cf2768165781c9ba36365e1bf
deep1man3 <pasha26k@ya.ru>
2021-07-15 11:29:24 +0300

    Добавляет height и width для `WithProgressionTaskIcon`

--- src/components/assets/WithProgressionTaskIcon/WithProgressionTaskIcon.tsx
+++ src/components/assets/WithProgressionTaskIcon/WithProgressionTaskIcon.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const WithProgressionTaskIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 95 63">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 95 63"
+    height="94"
+    width="61"
+  >
     <path
       fill="#323232"
       d="M7.48 62.057L35.73 33.76l18.833 18.833 40.02-45.011L87.945.943 54.563 38.468 35.728 19.635.417 54.995l7.062 7.062z"
