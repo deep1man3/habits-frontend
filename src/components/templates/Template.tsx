db32b7bcad58adc532dbb0242b674b96aa9340a0
deep1man3 <pasha26k@ya.ru>
2021-07-16 11:02:29 +0300

    Удаляет `Template.tsx` из папки с шаблонами

--- src/components/templates/Template.tsx
+++
@@ -1,8 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import BaseTemplate from './Base';
-
-const Template = ({ children }: PropsWithChildren<any>) => (
-  <BaseTemplate>{children}</BaseTemplate>
-);
-
-export default Template;
