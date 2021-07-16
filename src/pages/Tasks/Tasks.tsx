17c9b32a371428aba693ec2dd99dcabf14fda605
deep1man3 <pasha26k@ya.ru>
2021-07-16 13:31:16 +0300

    Перемещает компонент `CenteredPageTitle` в шаблон `Dashboard`

--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
-import CentredPageTitle from '../../components/lowLevel/CentredPageContent';
+import CentredPageTitle from '../../components/templates/Dashboard/CentredPageTitle';
 import HabitsList from '../../components/templates/Dashboard/HabitsList';
 
 const Tasks = () => (
   <>
     <CentredPageTitle>
-      <Typography variant="h4">Задания на сегодня</Typography>
+      Задания на сегодня
     </CentredPageTitle>
     <HabitsList />
   </>
