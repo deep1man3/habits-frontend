c24e3db34eb110dbab74b5e0461a3ad641c8123c
deep1man3 <pasha26k@ya.ru>
2021-07-16 13:22:39 +0300

    Добавляет комментарий в `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import store from './store';
 import Router from './routing';
 
 const App = () => {
+  /**
+   * TODO Перенести это в Preloader/InitializeApp?
+   * Поскольку нет ендпоинта на получения пользователя из токена (getUser)
+   * при каждой перезагрузки приложения через F5, redux очищается
+   * и остается токен в localStorage. Временное решение этой проблемы
+   * очищения localStorage.clear().
+   */
   useEffect(() => {
     localStorage.clear();
   }, []);
