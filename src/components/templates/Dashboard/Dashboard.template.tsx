02f649962ed7bace3f41755c34b36b5d29794585
deep1man3 <pasha26k@ya.ru>
2021-07-16 11:32:36 +0300

    Добавляет `AppBar` в шаблон `Dashboard` на разрешениях меньши lg

--- src/components/templates/Dashboard/Dashboard.template.tsx
+++ src/components/templates/Dashboard/Dashboard.template.tsx
@@ -1,10 +1,30 @@
 import React, { PropsWithChildren } from 'react';
+import { Hidden, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useDispatch } from 'react-redux';
+import Drawer from './Drawer';
+import AppBar from '../../lowLevel/AppBar';
+import { metaActions } from '../../../store/meta/slice';
 
-const DashboardTemplate = ({ children }: PropsWithChildren<unknown>) => (
-  <div>
-    Dashboard
-    {children}
-  </div>
-);
+const DashboardTemplate = ({ children }: PropsWithChildren<unknown>) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <Hidden lgUp>
+        <AppBar position="relative">
+          <IconButton
+            color="inherit"
+            onClick={() => dispatch(metaActions.toggleDrawerOpen())}
+          >
+            <MenuIcon />
+          </IconButton>
+        </AppBar>
+      </Hidden>
+      <Drawer>1</Drawer>
+      {children}
+    </div>
+  );
+};
 
 export default DashboardTemplate;
