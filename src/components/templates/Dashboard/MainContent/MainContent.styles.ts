5550257085f7b1c9d657c877a6adc3db795f3b6e
deep1man3 <pasha26k@ya.ru>
2021-07-19 16:27:47 +0300

    Изменяет у `MainContent` padding на разрешениях меньше md

--- src/components/templates/Dashboard/MainContent/MainContent.styles.ts
+++ src/components/templates/Dashboard/MainContent/MainContent.styles.ts
@@ -4,6 +4,7 @@ export const useStyles = makeStyles((theme) => ({
   root: {
     [theme.breakpoints.down('md')]: {
       margin: theme.spacing(4, 0, 0, 0),
+      padding: theme.spacing(0),
     },
     padding: theme.spacing(2, 8),
     marginLeft: 240 + theme.spacing(8),
