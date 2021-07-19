84ce111f8a5c094d7868c12c052dc106ee891f2d
deep1man3 <pasha26k@ya.ru>
2021-07-19 16:15:55 +0300

    Добавляет метод `getTasks` в `TaskService` для получения списка выполненных задач

--- src/utils/services/TaskService/TaskService.ts
+++ src/utils/services/TaskService/TaskService.ts
@@ -8,11 +8,17 @@ import { tasksActions } from '../../../store/tasks/slice';
 
 export default class TaskService {
   static async createTask(data: CreateTaskDTO): Promise<void> {
-    await $api.post<CreateTaskDTO>('/tasks/create', data);
+    if (localStorage.getItem('habits:token')) {
+      await $api
+        .post<CreateTaskDTO>('/tasks/create', data)
+        .then(() => this.getTasks());
+    }
   }
 
-  static async getTask(): Promise<void> {
-    const response = await $api.get<TaskResponseDTO[]>('/tasks');
-    store.dispatch(tasksActions.setTasks(response.data));
+  static async getTasks(): Promise<void> {
+    if (localStorage.getItem('habits:token')) {
+      const response = await $api.get<TaskResponseDTO[]>('/tasks');
+      store.dispatch(tasksActions.setTasks(response.data));
+    }
   }
 }
