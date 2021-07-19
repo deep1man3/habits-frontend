620273b4d8920e35d3b03e3b4823099f47d938a4
deep1man3 <pasha26k@ya.ru>
2021-07-19 16:56:36 +0300

    Добавляет `theme.ts` палитру с цветами для `AchievementsTable`

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@material-ui/core';
 import store from './store';
 import Router from './routing';
 import Preloader from './components/lowLevel/Preloader';
+import theme from './theme';
 
 const App = () => (
   <Provider store={store}>
-    <Preloader>
-      <Router />
-    </Preloader>
+    <ThemeProvider theme={theme}>
+      <Preloader>
+        <Router />
+      </Preloader>
+    </ThemeProvider>
   </Provider>
 );
 
