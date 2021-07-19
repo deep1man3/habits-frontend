20cbdcf00b01a00fefc5a5e2eaa47fe83b9dd808
deep1man3 <pasha26k@ya.ru>
2021-07-19 14:38:36 +0300

    Убирает лишнее из createTask и добавляет метод getTasks

--- src/utils/services/TaskService/TaskService.ts
+++ src/utils/services/TaskService/TaskService.ts
@@ -1,15 +1,18 @@
 import $api from '../../api';
-import { CreateTaskDTO } from '../../../types/DTO/TaskResponseDTO';
+import {
+  CreateTaskDTO,
+  TaskResponseDTO,
+} from '../../../types/DTO/TaskResponseDTO';
+import store from '../../../store';
+import { tasksActions } from '../../../store/tasks/slice';
 
 export default class TaskService {
-  static async createTask(
-    data: CreateTaskDTO,
-    onSuccess?: () => void,
-    onFinally?: () => void
-  ): Promise<void> {
-    await $api.post<CreateTaskDTO>('/tasks/create', data).finally(() => {
-      onFinally && onFinally();
-    });
-    onSuccess && onSuccess();
+  static async createTask(data: CreateTaskDTO): Promise<void> {
+    await $api.post<CreateTaskDTO>('/tasks/create', data);
+  }
+
+  static async getTask(): Promise<void> {
+    const response = await $api.get<TaskResponseDTO[]>('/tasks');
+    store.dispatch(tasksActions.setTasks(response.data));
   }
 }
