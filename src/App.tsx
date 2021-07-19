0d31ef561f3b0f24ea366a2b5c0883f80ff353e9
deep1man3 <pasha26k@ya.ru>
2021-07-19 13:40:33 +0300

    Убирает комметарий и useEffect из app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { Provider } from 'react-redux';
 import store from './store';
 import Router from './routing';
 
-const App = () => {
-  /**
-   * TODO Перенести это в Preloader/InitializeApp?
-   * Поскольку нет ендпоинта на получения пользователя из токена (getUser)
-   * при каждой перезагрузки приложения через F5, redux очищается
-   * и остается токен в localStorage. Временное решение этой проблемы
-   * очищения localStorage.clear().
-   */
-  useEffect(() => {
-    localStorage.clear();
-  }, []);
-
-  return (
-    <Provider store={store}>
-      <CssBaseline />
-      <Router />
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <CssBaseline />
+    <Router />
+  </Provider>
+);
 
 export default App;
