b09ebe2171838edd6dd08482f594509d7eea8ba6
deep1man3 <pasha26k@ya.ru>
2021-07-19 14:27:34 +0300

    Убирает получения Habits из `HabitList`

--- src/components/templates/Dashboard/HabitsList/HabitsList.tsx
+++ src/components/templates/Dashboard/HabitsList/HabitsList.tsx
@@ -1,17 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
-import HabitsService from '../../../../utils/services/HabitsService';
 import { useSelector } from '../../../../store';
 import HabitItem from '../HabitItem';
 
 const HabitsList = () => {
   const { habits } = useSelector((state) => state.habits);
-
-  useEffect(() => {
-    if (localStorage.getItem('habits:token')) {
-      HabitsService.getHabits();
-    }
-  }, []);
 
   if (!habits) {
     return (
