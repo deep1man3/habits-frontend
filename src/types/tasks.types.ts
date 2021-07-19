d95079724e4fb2aa3a89ddbfd8978b25e1e89c52
deep1man3 <pasha26k@ya.ru>
2021-07-19 14:37:13 +0300

    Добавляет интерфейсы для `Tasks`

---
+++ src/types/tasks.types.ts
@@ -0,0 +1,13 @@
+import { Habit } from './habits.types';
+
+export interface TasksState {
+  tasks: Task[] | null;
+}
+
+export interface Task {
+  id: number;
+  done: boolean;
+  donePercent: number | null;
+  completeDate: Date | null;
+  habit: Habit;
+}
