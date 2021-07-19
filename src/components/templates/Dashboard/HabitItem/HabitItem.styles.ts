4f48bb513f2926cd7967c2cc8c037d01d56acfac
deep1man3 <pasha26k@ya.ru>
2021-07-19 16:18:52 +0300

    Изменяет функцию `useStyles` и добавляет параметр isDone

--- src/components/templates/Dashboard/HabitItem/HabitItem.styles.ts
+++ src/components/templates/Dashboard/HabitItem/HabitItem.styles.ts
@@ -1,39 +1,45 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(2),
-    width: '100%',
-    [theme.breakpoints.down('sm')]: {
-      textAlign: 'center',
+export const useStyles = (isDone: boolean) =>
+  makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      marginBottom: theme.spacing(2),
+      order: isDone ? 0 : -1,
     },
-    marginBottom: theme.spacing(2),
-  },
-  icon: {
-    fontSize: '128px',
-    [theme.breakpoints.down('md')]: {
-      fontSize: '64px',
+    paper: {
+      padding: theme.spacing(2),
+      [theme.breakpoints.down('sm')]: {
+        textAlign: 'center',
+      },
+      width: '100%',
+      opacity: isDone ? '0.3' : '1',
     },
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '32px',
-    },
-  },
-  label: {
-    marginBottom: theme.spacing(2),
-    width: '100%',
-    '& h4': {
+    icon: {
+      fontSize: '128px',
       [theme.breakpoints.down('md')]: {
-        fontSize: '1.75rem',
+        fontSize: '64px',
       },
       [theme.breakpoints.down('sm')]: {
-        fontSize: '1.25rem',
+        fontSize: '32px',
       },
     },
-  },
-  description: {
-    width: '100%',
-  },
-  task: {
-    minHeight: '150px',
-  },
-}));
+    label: {
+      marginBottom: theme.spacing(2),
+      width: '100%',
+      '& h4': {
+        [theme.breakpoints.down('md')]: {
+          fontSize: '1.75rem',
+        },
+        [theme.breakpoints.down('sm')]: {
+          fontSize: '1.25rem',
+        },
+      },
+    },
+    description: {
+      width: '100%',
+    },
+    task: {
+      minHeight: '150px',
+    },
+  }));
