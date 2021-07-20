3f2138b1f45745ff04fe954e7b3c5df6492cbd16
deep1man3 <pasha26k@ya.ru>
2021-07-20 14:30:07 +0300

    Убирает логику получения массива выполненых habits из `HabitsList`

--- src/components/templates/Dashboard/HabitsList/HabitsList.tsx
+++ src/components/templates/Dashboard/HabitsList/HabitsList.tsx
@@ -1,33 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import { useSelector } from '../../../../store';
 import HabitItem from '../HabitItem';
 
 const HabitsList = () => {
-  const [idCompletedHabits, setIdCompletedHabits] = useState<Array<number>>([]);
-  const { habits } = useSelector((state) => state.habits);
-  const { tasks } = useSelector((state) => state.tasks);
-
-  const isToday = (someDate: string) => {
-    const taskDate = new Date(someDate?.split('T')[0]);
-    const today = new Date();
-    return (
-      taskDate.getDate() === today.getDate() &&
-      taskDate.getMonth() === today.getMonth() &&
-      taskDate.getFullYear() === today.getFullYear()
-    );
-  };
-
-  useEffect(() => {
-    setIdCompletedHabits(
-      tasks?.reduce((result: number[], task) => {
-        if (isToday(task.completeDate)) {
-          result.push(task.habit.id);
-        }
-        return result;
-      }, []) || []
-    );
-  }, [tasks]);
+  const { habits, habitsIdCompletedToday } = useSelector(
+    (state) => state.habits
+  );
 
   if (!habits) {
     return (
@@ -45,7 +24,7 @@ const HabitsList = () => {
           <HabitItem
             key={habit.id}
             habit={habit}
-            isDone={idCompletedHabits.includes(habit.id)}
+            isDone={habitsIdCompletedToday.includes(habit.id)}
           />
         ))}
     </Grid>
