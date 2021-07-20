766303e235bfddc2c38af85870aa2fcd9a7ea0a2
deep1man3 <pasha26k@ya.ru>
2021-07-20 17:39:35 +0300

    Добавляет запрос на получения measure в шаблон Dashboard

--- src/components/templates/Dashboard/Dashboard.container.tsx
+++ src/components/templates/Dashboard/Dashboard.container.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren, useEffect } from 'react';
 import DashboardTemplate from './Dashboard.template';
 import HabitService from '../../../utils/services/HabitService/HabitService';
 import TaskService from '../../../utils/services/TaskService';
+import MeasureService from '../../../utils/services/MeasureService';
 
 const DashboardContainer = ({ children }: PropsWithChildren<unknown>) => {
   useEffect(() => {
@@ -9,6 +10,8 @@ const DashboardContainer = ({ children }: PropsWithChildren<unknown>) => {
     TaskService.getTasks();
     // Получения habits пользователя
     HabitService.getHabits();
+    // Получения measures
+    MeasureService.getMeasures();
   }, []);
 
   return <DashboardTemplate>{children}</DashboardTemplate>;
